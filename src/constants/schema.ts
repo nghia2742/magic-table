d52b6a9ea3aee87d5ce868b7042af6e9244b7667
nghia.nt <nghia.nt2704@gmail.com>
2025-05-11 15:05:07 +0700

    refactor: chore schema

--- src/constants/schema.ts
+++ src/constants/schema.ts
@@ -8,9 +8,7 @@ export const AccountSchema = z.object({
     _id: z.string(),
     firstName: z.string().nonempty(MESSAGES.required),
     lastName: z.string().nonempty(MESSAGES.required),
-    age: z.number({ message: MESSAGES.number }).min(1, MESSAGES.required).refine(value => typeof value !== 'number', {
-        message: MESSAGES.number
-    }),
+    age: z.number({ message: MESSAGES.number }).min(1, MESSAGES.required),
     email: z.string().nonempty(MESSAGES.required),
     gender: z.string().nonempty(MESSAGES.required),
 })
